990087da2b00115528570834b571408c5da0e42b
nrif_ <naftaryandikaisyacafahmi@gmail.com>
2024-06-21 17:57:55 +0700

    purchase

--- application/views/purchase/input.php
+++ application/views/purchase/input.php
@@ -1,19 +1,54 @@
 <div class="col-lg-12">
     <div class="card shadow">
         <div class="card-header">
-            <h6 class="m-0 font-weight-bold text-primary">Input</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Header</h6>
         </div>
         <div class="card-body">
-            <div class="form-group row">
-                <label for="inId" class="col-sm-3 col-form-label">ID</label>
-                <div class="col-sm-9">
-                    <input type="text" class="form-control" id="inId" name="inId" required>
+            <div class="col-6">
+                <div class="form-group row">
+                    <label for="inId" class="col-sm-3 col-form-label">ID</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="inId" name="inId" required>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="inName" class="col-sm-3 col-form-label">Type</label>
-                <div class="col-sm-9">
-                    <input type="text" class="form-control" id="inId" name="inId" required>
+                <div class="form-group row">
+                    <label for="inDate" class="col-sm-3 col-form-label">Date</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="inDate" name="inDate" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inName" class="col-sm-3 col-form-label">Type</label>
+                    <div class="col-sm-9">
+                        <select class="form-control inType" style="width: 100%;">
+                            <option value="=">Barang</option>
+                            <option value="like">Jasa</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inSupplier" class="col-sm-3 col-form-label">Supplier</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="inSupplier" name="inSupplier" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inDuedate" class="col-sm-3 col-form-label">Due Date</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="inDuedate" name="inDuedate" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inTotal" class="col-sm-3 col-form-label">Total</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="inTotal" name="inTotal" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inRemark" class="col-sm-3 col-form-label">Remark</label>
+                    <div class="col-sm-9">
+                        <textarea type="text" class="form-control" id="inRemark" name="inRemark"></textarea>
+                    </div>
                 </div>
             </div>
         </div>
