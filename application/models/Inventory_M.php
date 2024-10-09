e6718c8a2f2ffa0d513b748e8cb8c633dccb3a36
nrif_ <naftaryandikaisyacafahmi@gmail.com>
2024-10-09 23:31:03 +0700

    inventory - add search transaction

--- application/models/Inventory_M.php
+++ application/models/Inventory_M.php
@@ -137,14 +137,14 @@ class Inventory_M extends CI_Model
                                     `status` <> 0 AND 
                                     inventory_type_id = 1 AND 
                                     inventory_id = '" . $inTransaction . "'
-                                )dt1 JOIN (
+                                )t1 JOIN (
                                     SELECT id, inventory_id, goods_id, qty, unit_id, `status` 
                                     FROM t_inventory_detail
                                     WHERE 
                                     `status` <> 0 AND 
                                     inventory_id = '" . $inTransaction . "'
-                                )dt2
-                                ON dt1.inventory_id = dt2.inventory_id";
+                                )t2
+                                ON t1.inventory_id = t2.inventory_id";
 
                     $row = $this->db->query($query)->num_rows();
 
@@ -155,36 +155,28 @@ class Inventory_M extends CI_Model
                         $data["res"] = [];
 
                         $query2 = "SELECT 
-                                        t1.purchase_id,
-                                        DATE_FORMAT(t1.`date`, '%d-%m-%Y ') AS `date`,
-                                    DATE_FORMAT(t1.due_date, '%d-%m-%Y ') AS `due_date`,
-                                    (SELECT `type` FROM m_purchase_type WHERE id = t1.purchase_type_id) AS `type`,
-                                    (SELECT supplier FROM m_supplier WHERE id = t1.supplier_id) AS supplier,
-                                    (SELECT currency FROM m_currency WHERE id = t1.currency_id) AS currency,
-                                    IF(t1.tax_type = 1 , 'Include (PKP)', 'Exclude (Non - PKP)') AS tax_type,
-                                        (SELECT goods FROM m_goods WHERE id = t2.goods_id) AS goods,
-                                        t2.qty,
-                                        t2.qty_received,  
-                                        (SELECT unit FROM m_unit WHERE id = t2.unit_id) AS unit
+                                    *, 
+                                    (SELECT `type` FROM m_inventory_type WHERE id = dt1.inventory_type_id) AS `type`,
+                                    (SELECT warehouse FROM m_warehouse WHERE id = dt1.warehouse_id) AS warehouse,
+                                    (SELECT goods FROM m_goods WHERE id = goods_id) AS goods,
+                                    DATE_FORMAT(`date`, '%d-%m-%Y ') AS `date`
                                     FROM (
                                         SELECT 
-                                        id, purchase_id, `date`, purchase_type_id, supplier_id, due_date, currency_id, tax_type
-                                        FROM t_purchase
+                                            id, inventory_id, `date`, inventory_type_id, warehouse_id, transaction_id, `status` 
+                                        FROM t_inventory 
                                         WHERE 
                                         `status` <> 0 AND 
-                                        purchase_id  LIKE '%" . $inTransaction . "%'
-                                    )t1 
-                                    LEFT  JOIN ( 
-                                        SELECT 
-                                        id, purchase_id, goods_id, qty, unit_id, qty_received
-                                        FROM t_purchase_detail
+                                        inventory_type_id = 1 AND 
+                                        inventory_id LIKE '%" . $inTransaction . "%'
+                                    )t1 JOIN (
+                                        SELECT id, inventory_id, goods_id, qty, unit_id, `status` 
+                                        FROM t_inventory_detail
                                         WHERE 
-                                        `status` <> 0  AND 
-                                        purchase_id  LIKE '%" . $inTransaction . "%'  AND 
-                                        (qty_received IS NULL OR qty > qty_received)
-                                    )t2 ON 
-                                    t1.purchase_id = t2.purchase_id
-                                    ORDER BY t1.date DESC";
+                                        `status` <> 0 AND 
+                                        inventory_id LIKE '%" . $inTransaction . "%'
+                                    )t2
+                                    ON t1.inventory_id = t2.inventory_id 
+                                    ORDER by t1.`date` DESC";
 
                         // return ($query2);
 
